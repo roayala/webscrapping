dd8b93bdd770793e2e549a2740ccd7baf0ff59f6
roas15 <sevilla_raul@hotmail.com>
2019-08-28 21:52:52 -0500

    Web page

--- scrape/scrape_mars.py
+++ scrape/scrape_mars.py
@@ -8,13 +8,15 @@ from splinter import Browser
 from splinter.exceptions import ElementDoesNotExist
 import pandas as pd
 
-# !which chromedriver
-executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
-browser = Browser("chrome", **executable_path, headless=False)
+
+def init_browser():
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
 
 
-def news_mars():
-
+def scrape():
+    # INITS BROWSER
+    browser = init_browser()
     url_nasa_mars_news = "https://mars.nasa.gov/news/"
     browser.visit(url_nasa_mars_news)
 
@@ -24,11 +26,7 @@ def news_mars():
     news_title = soup.body.find_all("div", class_="content_title")[0].text
     news_p = soup.body.find_all("div", class_="article_teaser_body")[0].text
 
-    return (news_title, news_p)
-
-
-def mars_images():
-
+    # IMAGE
     url_nasa_image = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
     browser.visit(url_nasa_image)
 
@@ -48,21 +46,19 @@ def mars_images():
     img_string = article[0].attrs["style"]
     featured_image_url = "https://www.jpl.nasa.gov" + img_string[index_img_url + 5 : -3]
 
-    return featured_image_url
+    #   return featured_image_url
 
-
-def mars_weather():
     url_mars_twitter = "https://twitter.com/marswxreport?lang=en"
     browser.visit(url_mars_twitter)
+
     html = browser.html
     soup = bs(html, "html.parser")
     body = soup.body.find_all("div", class_="js-tweet-text-container")[0]
     mars_weather = body.find_all("p")[0].text
     mars_weather = mars_weather[:-28]
-    return mars_weather
 
+    # return mars_weather
 
-def mars_facts():
     url_mars_facts = "https://space-facts.com/mars/"
     browser.visit(url_mars_facts)
 
@@ -90,10 +86,8 @@ def mars_facts():
         mars_data_dict[title] = mars_data[0][1][n]
         n = n + 1
 
-    return mars_data_dict
+    # return mars_data_dict
 
-
-def mars_hemispheres():
     url_mars_hemispheres = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url_mars_hemispheres)
 
@@ -113,36 +107,31 @@ def mars_hemispheres():
 
         browser.visit(url_mars_hemispheres)
 
-    return hemisphere_image_urls
+    # return hemisphere_image_urls
 
+    x, y = news_title, news_p
 
-def scrape():
+    z = featured_image_url
 
-    x, y = news_mars()
+    v = mars_weather
 
-    z = mars_images()
+    u = mars_data_dict
 
-    v = mars_weather()
-
-    u = mars_facts()
-
-    t = mars_hemispheres()
+    t = hemisphere_image_urls
 
     mars_scrape = [
         {
             "Title": x,
             "Extract": y,
-            "Featured Image": z,
-            "Mars Weather": v,
-            "Mars Facts": u,
-            "Mars Hemispheres": t,
+            "FeaturedImage": z,
+            "MarsWeather": v,
+            "MarsFacts": u,
+            "MarsHemispheres": t,
         }
     ]
 
     return mars_scrape
 
-
-# print(f"Mars Scrape: \n {scrape()}")
 
 # print(f"Title: {x} \nExtract: {y}")
 # print(f"Featured Image: {z}")
